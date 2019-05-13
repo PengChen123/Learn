7b91d9a4e93dc9401e397cc8fd5caaa8706f6219
Peng Chen <20632564@qq.com>
2019-05-13 17:23:29 +0800

    commit c# code

---
+++ code/c#/learn/learn/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //翻转'char 数据的  单词顺序'
+            char[] chars = "Hello World BeiJing".ToCharArray();
+            ReverseCharsWorld.ReverseWorld.Reverse(ref chars);
+            Console.WriteLine(new string(chars));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ code/c#/learn/learn/ReverseCharsWorld/ReverseWorld.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn.ReverseCharsWorld
+{
+    public static class ReverseWorld
+    {
+        public static void Reverse(ref char[] chars) {
+            if (chars == null) {
+                chars = new char[]{};
+            }
+            
+            int len = chars.Length;
+            Reverse(ref chars, 0, len - 1);
+            int start = 0;
+            int end = 0;
+            bool setStart = false;
+            for (int i = 0; i < len; i++) {
+                if (!setStart) {
+                    if (chars[i] != ' ') {
+                        start = i;
+                        setStart = true;
+                    }
+                }
+                if (chars[i] == ' ' || i == len-1) {
+                    if (i != len - 1){
+                        end = i - 1;
+                    }else {
+                        end = i;
+                    }
+                    Reverse(ref chars, start, end);
+                    setStart = false;
+                }
+            }
+        }
+        private static void Reverse(ref char[] chars,int i,int j) {
+            char c = '\0';
+            while (i < j) {
+                c = chars[i];
+                chars[i] = chars[j];
+                chars[j] = c;
+                i++;
+                j--;
+            }
+        }
+    }
+}
